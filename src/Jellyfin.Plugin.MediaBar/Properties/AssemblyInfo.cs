879dc83d5c5e3b06ab83aade5b2ecf5ff3fb5569
Jycreyn <jycreyn@users.noreply.github.com>
2026-04-22 16:10:10 +0200

    Revert "Fix assembly version to 2.5.1.0 and update build.yaml"
    This reverts commit a6af62988e1dcac80472c174a8b02cb01ab96802.

--- src/Jellyfin.Plugin.MediaBar/Properties/AssemblyInfo.cs
+++ src/Jellyfin.Plugin.MediaBar/Properties/AssemblyInfo.cs
@@ -1,9 +1,9 @@
 ﻿using System.Reflection;
 using Jellyfin.Plugin.MediaBar.Attributes;
 
-[assembly: AssemblyCompany("Jycreyn")]
+[assembly: AssemblyCompany("IAmParadox27")]
 [assembly: AssemblyProduct("Jellyfin.Plugins.MediaBar")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyTitle("Jellyfin.Plugins.MediaBar")]
-[assembly: AssemblyVersion("2.5.1.0")]
+[assembly: AssemblyVersion("2.4.0.0")]
 [assembly: JellyfinVersion("10.11.2")]
